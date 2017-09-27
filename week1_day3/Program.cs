c5334fd6245e275c3de3d647aee8610ba0aaaf9a
yusuf-farah <yfarah1577@gmail.com>
2017-09-27 13:39:37 -0400

    practice with age if -else

--- week1_day3/Program.cs
+++ week1_day3/Program.cs
@@ -21,10 +21,36 @@ namespace week1_day3
             {
                 // do something else
                 Console.WriteLine(" that makes sense");
-                
+
+
+
+               
+            }
+
+            Console.WriteLine(" what day is today");
+            string userAnswer = Console.ReadLine();
+            if (userAnswer == "Monday")
+            {
+                Console.WriteLine("Back to work");
+               }
+
+            else if (userAnswer == "Friday")
+                {
+                Console.WriteLine("happy friday");
 
             }
 
+            Console.WriteLine("how old are you?");
+            int userAage = int.Parse(Console.ReadLine());
+            if (userAge <= 1)
+            { 
+                Console.WriteLine("yay toddlers!");
         }
+        else if (userAge >= 4 &&  userAge < 4)
+            Console.WriteLine(" Enjoy childhood");
+
     }
+
+
+
 }
